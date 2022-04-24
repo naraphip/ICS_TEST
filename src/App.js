d84de83b84d73a187f53f921d778aee002486722
naraphip <naraphip.jin@gmail.com>
2022-04-24 20:48:39 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from './Page/Home';
 import Navbar from './Navbar/Navbar';
 import SingleCard from './Page/SinglePage';
@@ -7,13 +7,13 @@ import SingleCard from './Page/SinglePage';
 
 function App() {
   return (
-    <BrowserRouter basename='/ICS'>
+    <HashRouter basename='/ICS'>
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/post/:id" component={SingleCard} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
